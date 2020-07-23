85d10366121b0eac0dc0ea70e4d889678b42a9ee
Danie Audatte <danieaudatte@Danies-MacBook-Pro.local>
2020-07-22 20:26:42 -0400

    puma files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
+import App from './containers/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<App />,
+  // <React.StrictMode>
+  //
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
