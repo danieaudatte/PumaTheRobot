3b45f98ca2f6f9939e0ba9a90996e654bced825c
Danie Audatte <danieaudatte@Danies-MacBook-Pro.local>
2020-07-26 14:06:24 -0400

    Updates

---
+++ precache-manifest.679676f96c11fd068033959ed72a4e4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a4cfe605ef2e906633352a3349a900",
+    "url": "/PumaTheRobot/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/PumaTheRobot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ed682237cef48b5fba26",
+    "url": "/PumaTheRobot/static/css/main.ea23231e.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/PumaTheRobot/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PumaTheRobot/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed682237cef48b5fba26",
+    "url": "/PumaTheRobot/static/js/main.ee050f1c.chunk.js"
+  },
+  {
+    "revision": "b96fb5961059dc8bc7d3",
+    "url": "/PumaTheRobot/static/js/runtime-main.dcb7a6c9.js"
+  }
+]);
